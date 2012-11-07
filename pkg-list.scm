9d6cb925e518efe03f8759a7a0d4e519cd7d392a
Ian Price <ianprice90@googlemail.com>
2012-11-07 20:47:14 +0000

    Add pkg-list.scm

---
+++ pkg-list.scm
@@ -0,0 +1,13 @@
+(package (minikanren (0 1))
+  (synopsis "A relational programming extension to Scheme")
+  (description
+   "An R6RS packaged version of the MiniKanren logic programming"
+   "system, originally written by Friedman Byrd and Kiselyov, and"
+   "featured in the book, \"The Reasoned Schemer\".")
+  (homepage "http://github.com/ijp/minikanren/")
+  (libraries
+   "minikanren.scm"
+   "minikanren")
+  (documentation
+   "README.org"
+   "COPYING"))
