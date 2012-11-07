137e7b8639e5bd6020e01d87b7eb7619b81643f6
Ian Price <ianprice90@googlemail.com>
2012-11-07 20:11:01 +0000

    Add minikanren.scm

---
+++ minikanren.scm
@@ -0,0 +1,54 @@
+#!r6rs
+(library (minikanren)
+(export run
+        succeed
+        fail
+        ==
+        ==-check
+        fresh
+        conde
+        all
+        alli
+        condi
+        conda
+        condu
+        ife
+        ifi
+        ifa
+        ifu
+        run*
+        lambda-limited
+       
+        caro
+        cdro
+        conso
+        nullo
+        eqo
+        eq-caro
+        pairo
+        listo
+        membero
+        rembero
+        appendo
+        anyo
+        nevero
+        alwayso
+
+        build-num
+        poso
+        >1o
+        +o
+        -o
+        *o
+        =lo
+        <lo
+        <=lo
+        /o
+        logo
+        expo
+        trace-vars
+        )
+(import (minikanren mk)
+        (minikanren mkextraforms)
+        (minikanren mkprelude))
+)
